b3ab81959cf048804d3b57b0930a8893948f4674
Eitan Halper-Stromberg <Eitan177@gmail.com>
2023-11-21 16:03:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,21 @@ st.set_page_config(layout='wide')
 st.title('Plot allele frequency column from vcf file')
 mvfe=st.file_uploader('Upload master variant file extreme here')
 onlysnps=st.checkbox('only SNPs')
-if onlysnps:
-    mvfe=mvfe.iloc[np.where(mvfe['rsID'].str.contains('rs'))[0]]
+usegenomicCoordinate=st.checkboc('Use genomic coordinates instead of indices')
+
 if mvfe != None:
     chart_data = pd.read_table(mvfe,sep='\t')
-
+    if onlysnps:
+        mvfe=mvfe.iloc[np.where(mvfe['rsID'].str.contains('rs'))[0]]
     # Display a scatterplot chart
     cola,colb,colc= st.columns(3)
     m=0
     for a in np.unique(chart_data['CHROM']):
         chrom = chart_data[chart_data['CHROM']==a]
-        chrom['ind']=np.arange(0,chrom.shape[0])
+        if not usegenomicCoordinate:
+            chrom['ind']=np.arange(0,chrom.shape[0])
+        else:
+            chrom['ind']=  chrom['POS']
         chrom['gene']=[str(y) for y in chrom['GENE']] 
         chrom.reset_index(inplace=True)
         if m % 3 == 0:
