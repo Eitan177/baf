6ccc70313267f862aafba1a1c579fb3297ed1222
Eitan Halper-Stromberg <Eitan177@gmail.com>
2023-12-14 14:20:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,21 @@ import pandas as pd
 import numpy as np
 st.set_page_config(layout='wide')
 st.title('Plot allele frequency column from vcf file')
-mvfe=st.file_uploader('Upload master variant file extreme here')
+mvfe=st.file_uploader('Upload master variant file extreme here',type=['txt','maf'])
 onlysnps=st.checkbox('only SNPs')
 usegenomicCoordinate=st.checkbox('Use genomic coordinates instead of indices')
 
 if mvfe != None:
-    chart_data = pd.read_table(mvfe,sep='\t')
+    if uploaded_file.type == "text/csv":
+        st.write('reading text file')
+        chart_data = pd.read_table(mvfe,sep='\t')
+    else:
+        st.write('reading maf file')
+        chart_data = pd.read_csv(mvfe,sep='\t',skiprows=1)    
+        chart_data['AF']=chart_data['DP4'].str.split(',',expand=True).astype(int).apply(lambda x: x[2:4].sum()/x.sum(),axis=1) 
+        chart_data['POS']=chart_data['Start_Position']
+        chart_data['GENE']=chart_data['Hugo_Symbol']
+        chart_data['CHROM']=chart_data['Chromosome']
     if onlysnps:
         chart_data=chart_data.iloc[np.where(chart_data['rsID'].str.contains('rs'))[0]]
     # Display a scatterplot chart
