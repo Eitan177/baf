f914f23d7ef40e39a22c84245497ef3fcf9d5d70
Eitan Halper-Stromberg <Eitan177@gmail.com>
2024-12-13 21:02:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ if mvfe != None:
         st.write('reading maf file')
         chart_data = pd.read_csv(mvfe,sep='\t',skiprows=1)    
         try:
-            chart_data['AF']=chart_data['DP4'].apply(lambda x: x[2:4].sum()/x.sum(),axis=1) 
+            chart_data['AF']=chart_data['DP4'].str.split(',',expand=True).astype(int).apply(lambda x: x[2:4].sum()/x.sum(),axis=1) 
         except:
             chart_data['AF']=chart_data[['t_alt_count','t_depth']].apply(lambda x: x[0]/x[1],axis=1)
         chart_data['POS']=chart_data['Start_Position']
